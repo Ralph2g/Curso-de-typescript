a5c94327d6d5cd612846d8db3ccc89eab9afaca3
Rafael Garcia Garcia <Ralph2g@github.com>
2019-02-08 22:37:36 -0600

    Primeras pruebas

---
+++ salida.js
@@ -0,0 +1,15 @@
+console.log("hello-World");
+//types
+var myString = "Hello World";
+myString = 22 + "";
+var myNumber = 22;
+var myBoll = true;
+var myVar = "hello";
+myVar = false;
+document.write(myString);
+var StringArray = ["2", "item1", "item 2"];
+var StringNumber = [1, 3, 4];
+StringArray = [1, 3, 4];
+//tuple
+var strNumTuple;
+strNumTuple = ["hello", 22];
---
+++ salida.ts
@@ -0,0 +1,26 @@
+console.log("hello-World");
+
+
+//types
+var myString: string = "Hello World"
+
+myString = 22 + "";
+
+var myNumber:number = 22;
+var myBoll: boolean =true;
+
+var myVar:any = "hello";
+myVar = false;
+
+document.write(myString)
+
+var StringArray:any[] =[ "2","item1","item 2"];
+var StringNumber:number[] = [1,3,4];
+
+StringArray =[1,3,4];
+
+//tuple
+var strNumTuple:[string,number];
+strNumTuple = ["hello",22];
+
+
